01c177abdee0f71bdee1ada8fc60e65f73af6e49
JonsCodes <johnalyashai@me.com>
2023-06-03 17:22:33 -0700

    Form Data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,35 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
-    res.render('places/index', {places})
+
+// GET /places/new route
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.redirect('POST /places')
 })
 
-module.exports = router
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
+
+  res.render("places/index", { places });
+});
+
+module.exports = router;
