87fb6dc5f4c2d47a64ba99647791a70913d4fd2c
JonsCodes <johnalyashai@me.com>
2023-06-03 18:33:42 -0700

    Fixed Bootstrap and CSS issue

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,12 @@ function Def (html) {
         <html>
            <head>
                 <title>Title</title>
-                <link rel='stylesheet' href='/css/style.css'/>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"/>
+                <link rel='stylesheet' href='/css/styles.css'/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
             </head>
             <body>
                 {html.children}
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
             </body>
         </html>
     )
