93ecce7b25b2e4ca31de3b34c8bd5341dd3fa116
JonsCodes <johnalyashai@me.com>
2023-06-05 19:35:07 -0700

    Navi with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,19 +1,36 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-           <head>
-                <title>Title</title>
-                <link rel='stylesheet' href='/css/styles.css'/>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
-            </head>
-            <body>
-                {html.children}
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
-            </body>
-        </html>
-    )
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link rel="stylesheet" href="/css/styles.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        />
+      </head>
+      <body>
+        {html.children}
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+      </body>
+    </html>
+  );
 }
 
-module.exports = Def
+module.exports = Def;
