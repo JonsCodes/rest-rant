fbc850ee6fa17dd54ac02a98a1cb57a99aa8750f
JonsCodes <johnalyashai@me.com>
2023-06-17 15:41:31 -0700

    Edited edit.jsx

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,49 +1,51 @@
-const React = require('react')
-const Def = require('../default')
+const React = require('react');
+const Def = require('../default');
 
 function edit_form(data) {
-    return (
-        <Def>
-            <main>
-                <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-            <div className="row">
-                <div className="form-group col-sm-6">
-                    <label htmlFor="name">Place Name</label>
-                    <input className="form-control" id="name" name="name" value={data.place.name} required />
-                </div>
-                <div className="form-group col-sm-6">
-                    <label htmlFor="pic">Place Picture</label>
-                    <input className="form-control" id="pic" name="pic" />
-                </div>
+  const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+  return (
+    <Def>
+      <main>
+        <h1>Edit Place</h1>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+          <div className="row">
+            <div className="form-group col-sm-6">
+              <label htmlFor="name">Place Name</label>
+              <input className="form-control" id="name" name="name" value={data.place.name} required />
             </div>
-                <div className="form-group">
-                    <label htmlFor="city">City</label>
-                    <input className="form-control" id="city" name="city" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="state">State</label>
-                    <input className="form-control" id="state" name="state" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="cuisines">Cuisines</label>
-                    <input className="form-control" id="cuisines" name="cuisines" required />
-                </div>
-                <div className="form-group">
-                <label htmlFor="founded">Founded</label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="founded"
-                    name="founded"
-                    value={data.founded}/>
-                </div>
-                <input className="btn btn-primary" type="submit" value="Submit Changes" />
-            </form>
-            </main>
-        </Def>
-    )
+            <div className="form-group col-sm-6">
+              <label htmlFor="pic">Place Picture</label>
+              <input className="form-control" id="pic" name="pic" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input className="form-control" id="city" name="city" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <select className="form-control" id="state" name="state">
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="cuisines">Cuisines</label>
+            <input className="form-control" id="cuisines" name="cuisines" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="founded">Founded</label>
+            <input type="number" className="form-control" id="founded" name="founded" value={data.founded} />
+          </div>
+          <input className="btn btn-primary" type="submit" value="Submit Changes" />
+        </form>
+      </main>
+    </Def>
+  );
 }
 
-
-module.exports = edit_form
+module.exports = edit_form;
