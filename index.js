89d7affc9f94d1764984270955196a197780d00e
JonsCodes <johnalyashai@me.com>
2023-06-25 17:15:16 -0700

    editd index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
   () => {console.log('connected to : ', process.env.MONGO_URI)})
 
